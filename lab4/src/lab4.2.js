fe530c1603e41612addd7203d471430a8f95a570
Fwerfad <30774377+Fwerfad@users.noreply.github.com>
2020-04-28 14:54:17 +0300

    Add files via upload

--- lab4/src/lab4.2.js
+++ lab4/src/lab4.2.js
@@ -1,27 +1,26 @@
 async function Pokemon(url1) {
-    let response = await fetch(url1);
-    if (!response.ok) {
-        return false;
-    }
-    let json = await response.json();
-    const name = ("<span>" + "Покемон: " + json.forms[0].name + "<br>" + "</span>");
-    const abilities = ("<span>" + "Способности: " + json.abilities[0].ability.name + "<br>" + "</span>");
-    var effect = [];
-    for (let i = 0; i < json.abilities.length; i++) {
-        let response = await fetch(json.abilities[i].ability.url);
-        if (!response.ok) {
-            return false;
+    let [response] = await Promise.all([fetch(url1)]);
+    if (response.ok) {
+        let [json] = await Promise.all([response.json()]);
+        const name = ("<span>" + "Покемон: " + json.forms[0].name + "<br>" + "</span>"),
+            abilities = ("<span>" + "Способности: " + json.abilities[0].ability.name + "<br>" + "</span>");
+        let effect = [];
+        for (let i = 0; i < json.abilities.length; i++) {
+            let [response] = await Promise.all([fetch(json.abilities[i].ability.url)]);
+            if (response.ok) {
+                let json2 = await response.json();
+                effect += "<span>" + "Cпособность: " + (json2.effect_entries[0].effect) + "<br>" + "</span>";
+            } else return false;
         }
-        let json2 = await response.json();
-        effect += "<span>" + "Cпособность: " + (json2.effect_entries[0].effect) + "<br>" + "</span>";
+        let elem = document.getElementById("app");
+        elem.innerHTML = name + abilities + effect;
+        return true;
     }
-    let elem = document.getElementById("app");
-    elem.innerHTML = name + abilities + effect;
-    return true;
+    return false;
 }
 
 export default function getPokemonInfo(url) {
-    if (Pokemon(url) == false) {
+    if (!Pokemon(url)) {
         let elem = document.getElementById("app");
         elem.innerHTML = "ERROR";
     }
