439c91fc6a3fbd54cc95ad5865deecdefbdb60e9
Fwerfad <30774377+Fwerfad@users.noreply.github.com>
2020-04-28 17:46:10 +0300

    Add files via upload

--- lab4/src/lab4.2.js
+++ lab4/src/lab4.2.js
@@ -1,28 +1,24 @@
-async function Pokemon(url1) {
-    let [response] = await Promise.all([fetch(url1)]);
-    if (response.ok) {
-        let [json] = await Promise.all([response.json()]);
-        const name = ("<span>" + "Покемон: " + json.forms[0].name + "<br>" + "</span>"),
-            abilities = ("<span>" + "Способности: " + json.abilities[0].ability.name + "<br>" + "</span>");
-        let effect = [];
-        for (let i = 0; i < json.abilities.length; i++) {
-            let [response] = await Promise.all([fetch(json.abilities[i].ability.url)]);
-            if (response.ok) {
-                let json2 = await response.json();
-                effect += "<span>" + "Cпособность: " + (json2.effect_entries[0].effect) + "<br>" + "</span>";
-            } else return false;
+export default async function getPokemonInfo(url) {
+    try {
+        let [response] = await Promise.all([fetch(url)]);
+        if (response.ok) {
+            let [json] = await Promise.all([response.json()]);
+            const name = ("<span>" + "Покемон: " + json.forms[0].name + "<br>" + "</span>"),
+                abilities = ("<span>" + "Способности: " + json.abilities[0].ability.name + "<br>" + "</span>");
+            let effect = [];
+            for (let i = 0; i < json.abilities.length; i++) {
+                let [response] = await Promise.all([fetch(json.abilities[i].ability.url)]);
+                if (response.ok) {
+                    let json2 = await response.json();
+                    effect += "<span>" + "Cпособность: " + (json2.effect_entries[0].effect) + "<br>" + "</span>";
+                }
+            }
+            let elem = document.getElementById("app");
+            elem.innerHTML = name + abilities + effect;
         }
+    } catch (e) {
         let elem = document.getElementById("app");
-        elem.innerHTML = name + abilities + effect;
-        return true;
-    }
-    return false;
-}
-
-export default function getPokemonInfo(url) {
-    if (!Pokemon(url)) {
-        let elem = document.getElementById("app");
-        elem.innerHTML = "ERROR";
+        elem.innerHTML = "ERROR " + e.toString();
     }
 
 }
